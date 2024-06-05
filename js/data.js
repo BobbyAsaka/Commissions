4f7379c45955ad0c0d74ed360c044d05201033f7
BobbyAsaka <darkfyre545@gmail.com>
2024-06-05 16:36:56 -0700

    experimenting

--- js/data.js
+++ js/data.js
@@ -1,15 +1,28 @@
 var data = {
 	images: [
 		{
-			src: [""],
-			thumbnail: "",
-			title: "",
-			artist: "",
-			artist_url: null,
+			src: ["https://pbs.twimg.com/media/F6ghkX7akAAJn7Z?format=jpg&name=large"],
+			thumbnail: "https://pbs.twimg.com/media/F6ghkX7akAAJn7Z?format=jpg&name=large",
+			title: "Yukata Yukitsuki",
+			artist: "Philo",
+			artist_url: "https://twitter.com/Philo_324",
 			art_url: null,
-			desc: "",
-			date_str: "",
+			desc: "Commissioned to celebrate the release of Fragment's Note 2+.\nThe flower on the yukata is morning glory which is call asagao (朝顔アサガオ, meaning morning face）in Japanese. I made reference to the character's name Asaka (朝霞, meaning morning mist) .",
+			date_str: "September 20, 2023",
 			tags: [],
-			hidden: true
+			hidden: false
+		},
+		{
+			src: ["https://pbs.twimg.com/media/FSaSH7EWYAEK1Za?format=jpg&name=large"],
+			thumbnail: "https://pbs.twimg.com/media/FSaSH7EWYAEK1Za?format=jpg&name=large",
+			title: "Chandra from DLATMOL",
+			artist: "OniKagura",
+			artist_url: "https://twitter.com/kagura_oni",
+			art_url: "https://twitter.com/kagura_oni/status/1524066192100667396",
+			desc: "",
+			date_str: "May 10, 2022",
+			tags: [],
+			hidden: false
 		}
-		]}
+		]
+}
